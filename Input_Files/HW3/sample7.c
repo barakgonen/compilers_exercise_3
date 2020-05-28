9be3cc10a792b48e867e8c527021717eca8ec9ae
Barak Gonen <barakgonen9@gmail.com>
2020-05-28 13:50:45 +0300

    Input files changes
    floor && ceil

--- Input_Files/HW3/sample7.c
+++ Input_Files/HW3/sample7.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
 void main() {
-
-    // we have a bug with initialization in 1 line  but it looks like it's not important since it does not appear in
-    // other input files
     int a;
-    int b, c;
-    double dA, dB, dC;
-    a = 5;
-    a -= 0;
-    a += 0;
-    a /= 1;
-    a /= 0;
-    a *= 1;
-    a *= 14-12;
+    double b;
+    a = 7;
+    printf("%d\n", a);
+    a = 8.0;
+    printf("%d\n", a);
+    a = 0;
+    printf("%d\n", a);
+    a = 1;
+    printf("%d\n", a);
+    b = 1.6;
+    printf("%f\n", b);
+    a = 2 + (2 - 4);
+    printf("%d\n", a);
+    a = 2 + (5 + 7);
+    printf("%d\n", a);
+//    a -= 0;
+//    a += 0;
+//    a += 2 + (2 - 4); // should not write anything
+//    a /= 1;
+//    a /= 0;
+//    a *= 1;
+//    a *= 14-12;
+//
+//    a += (14 - 15) + (-1 + 2) / 1;
 
     return;
 }
